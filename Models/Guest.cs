912b278ecbe151f105615909b88a207605c5c4d1
singhamahapatramayna7-lang <singhamahapatramayna7@gmail.com>
2025-09-03 22:15:15 +0530

    :wq s

---
+++ Models/Guest.cs
@@ -0,0 +1,10 @@
+class Guest
+{
+public string Name{set;get;}
+public string Address{set;get;}
+public string phoneNumber{set;get;}
+public string allotedRoom{set;get;}
+public string rent{set;get;}
+
+
+}
