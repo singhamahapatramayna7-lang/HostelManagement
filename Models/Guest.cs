1fc3ab57c75176e4d95f7f24332e79097bfb3e2c
“[https://github.com/vmahesh77/mahesh_roject.git]” <[vemulapudimahesh123@gmail.com]>
2025-09-05 22:30:58 +0530

    implemented crud opratinons

--- Models/Guest.cs
+++ Models/Guest.cs
@@ -1,5 +1,11 @@
 class Guest
 {
 
-
+public int GuestId { get; set; }
+    public string Name { get; set; }
+    public string Phone { get; set; }
+    public int Age { get; set; }
+    public string RoomNumber { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime? CheckOutDate { get; set; }
 }
